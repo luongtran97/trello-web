38cb526b8960a72515adc542e3f3d5e851bc5059
luong tran <luongluong99783@gmail.com>
2023-11-25 09:05:43 +0700

    config_them

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
-
+import theme from './theme.js'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline/>
-    <App />
+    <CssVarsProvider theme={theme}>
+      <CssBaseline/>
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>
 )
