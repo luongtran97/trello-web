5eb2d7e4a0e6330e7a6ea7ad1595624d40ebabf4
luong tran <luongluong99783@gmail.com>
2023-11-25 21:54:24 +0700

    refactor_project_structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,9 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import { useColorScheme } from '@mui/material/styles'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeOutLineIcon from '@mui/icons-material/DarkMode'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import Box from '@mui/material/Box'
-function ModeSelect() {
+import Board from './pages/Board/_id'
 
-  const { mode, setMode } = useColorScheme()
-
-  const handleChange = (event) => {
-    const selectedMode = event.target.value
-    setMode(selectedMode)
-  }
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value="light">
-          <div style={{ display:'flex', alignItems:'center', gap:'8px' }}>
-            <LightModeIcon fontSize='small'/>
-            Light
-          </div>
-        </MenuItem>
-        <MenuItem value="dark">
-          <Box sx={{ display:'flex', alignItems:'center', gap:1 }}>
-            <DarkModeOutLineIcon fontSize='small'/>
-            Dark
-          </Box>
-        </MenuItem>
-        <MenuItem value="system">
-          <Box sx={{
-            display:'flex', alignItems:'center', gap:1
-          }}>
-            <SettingsBrightnessIcon fontSize='small'/>
-            System
-          </Box>
-        </MenuItem>
-      </Select>
-    </FormControl>
-  )
-}
-
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
 function App() {
-
-
   return (
     <>
-      <ModeSelect/>
-      <hr/>
-      <ModeToggle/>
-      <div className="luongTran">luongtrandev</div>
-      <Button variant="contained">Hello world</Button>
-      <Button variant="text">Text</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br/>
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
-      <HomeIcon />
-      <HomeIcon color='primary' />
-      <HomeIcon color='secondary' />
-      <HomeIcon color='success' />
-      <HomeIcon color='action' />
-      <HomeIcon color='disabled' />
-      <HomeIcon sx={{ color: pink[500] }} />
+      <Board/>
     </>
   )
 }
---
+++ src/utils/sort.js
@@ -0,0 +1,1 @@
+// sort colunms and carts
