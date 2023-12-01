98250f84aa4f3224fe7a1a70afe3fa5aaf29189d
luong tran <luongluong99783@gmail.com>
2023-12-01 23:24:26 +0700

    drag_drop_card_p2

--- src/pages/Board/BoardContent/List Columns/Columns/ListCards/Card/TrelloCard.jsx
+++ src/pages/Board/BoardContent/List Columns/Columns/ListCards/Card/TrelloCard.jsx
@@ -25,7 +25,8 @@ function TrelloCard({ card }) {
     // touchAction:'none', //dành cho sensor dạng default
     transform: CSS.Translate.toString(transform),
     transition,
-    opacity: isDragging ? 0.5 : undefined
+    opacity: isDragging ? 0.5 : undefined,
+    border: isDragging ? '1px solid #2ecc71' : undefined
   }
 
   const shouldShowCardAction = () => {
