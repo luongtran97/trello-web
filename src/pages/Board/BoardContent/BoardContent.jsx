6b46b048a131e30a505711ac96b8c36287265548
luong tran <luongluong99783@gmail.com>
2023-12-01 16:20:10 +0700

    check_drop_card-p1

--- src/pages/Board/BoardContent/BoardContent.jsx
+++ src/pages/Board/BoardContent/BoardContent.jsx
@@ -7,13 +7,20 @@ import {
   useSensor,
   useSensors,
   MouseSensor,
-  TouchSensor
+  TouchSensor,
+  DragOverlay
 } from '@dnd-kit/core'
 import { arrayMove } from '@dnd-kit/sortable'
+import Cloumn from './List Columns/Columns/Cloumn'
+import TrelloCard from './List Columns/Columns/ListCards/Card/TrelloCard'
+
+const ACTIVE_DRAG_ITEM_STYLE = {
+  COLUMN: 'ACTIVE_DRAG_ITEM_STYLE_COLUMN',
+  CARD:'ACTIVE_DRAG_ITEM_STYLE_CARD'
+}
 
 function BoardContent({ board }) {
   // const pointerSensor = useSensor(PointerSensor, { activationConstraint:{ distance:10 } })
-
   const mouseSensor = useSensor(MouseSensor, { activationConstraint:{ distance:10 } })
 
   // nhấn giữ 250s , và chênh lệch 5 px thì sẽ chạy hàm kích hoạt event
@@ -24,9 +31,21 @@ function BoardContent({ board }) {
 
   const [orderedColumns, setOrderedColumns] = useState([])
 
+  // cùng 1 thời điểm chỉ có 1 item được kéo là card hoặc column
+  const [activeDragItemId, setActiveDragItemId] = useState(null)
+
+
+  const [activeDragItemType, setActiveDragItemType] = useState(null)
+
+
+  const [activeDragItemData, setActiveDragItemData] = useState(null)
+
+
   useEffect(() => {
     setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
   }, [board])
+
+  // xử lý khi kết thúc kéo 1 phần tử (thả - drop)
   const handelDragEnd = (event) => {
     const { active, over } = event
     //kiểm tra nếu không tồn tại over kéo linh tinh ra ngoài tránh lỗi
@@ -38,10 +57,21 @@ function BoardContent({ board }) {
       const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
       setOrderedColumns(dndOrderedColumns)
     }
-  }
+    // set state về null khi (kéo  ) thả xong
+    setActiveDragItemId(null)
+    setActiveDragItemType(null)
+    setActiveDragItemData(null)
 
+  }
+  // xử lý bắt đầu kéo - drag
+  const handelDragStart = (event) => {
+    // khi bắt đầu kéo set sữ liệu, sau khi kéo xong phải set về null
+    setActiveDragItemId(event?.active?.id)
+    setActiveDragItemType(event?.active?.data?.current?.columnId ? ACTIVE_DRAG_ITEM_STYLE.CARD : ACTIVE_DRAG_ITEM_STYLE.COLUMN)
+    setActiveDragItemData(event?.active?.data?.current)
+  }
   return (
-    <DndContext onDragEnd={handelDragEnd} sensors={sensor}>
+    <DndContext onDragEnd={handelDragEnd} onDragStart={handelDragStart} sensors={sensor}>
       <Box sx={{
         bgcolor:(theme) => ( theme.palette.mode === 'dark' ? '#34495e' : '#1976d2' ),
         width:'100%',
@@ -53,6 +83,13 @@ function BoardContent({ board }) {
         p:'10px 0'
       }}>
         <ListColumns columns={orderedColumns} />
+        <DragOverlay dropAnimation={{
+          duration: 500,
+          easing: 'cubic-bezier(0.18, 0.67, 0.6, 1.22)' }}>
+          { !activeDragItemType && null }
+          { activeDragItemType === ACTIVE_DRAG_ITEM_STYLE.COLUMN && <Cloumn column={activeDragItemData}/>}
+          { activeDragItemType === ACTIVE_DRAG_ITEM_STYLE.CARD && <TrelloCard card={activeDragItemData}/>}
+        </DragOverlay>
       </Box>
 
     </DndContext>
--- src/pages/Board/BoardContent/List Columns/Columns/ListCards/Card/TrelloCard.jsx
+++ src/pages/Board/BoardContent/List Columns/Columns/ListCards/Card/TrelloCard.jsx
@@ -7,30 +7,54 @@ import CardMedia from '@mui/material/CardMedia'
 import GroupIcon from '@mui/icons-material/Group'
 import CommentIcon from '@mui/icons-material/Comment'
 import AttachmentIcon from '@mui/icons-material/Attachment'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 function TrelloCard({ card }) {
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging
+  } = useSortable({ id: card._id, data: { ...card } })
+
+  const dndKitColumnStyle = {
+    // touchAction:'none', //dành cho sensor dạng default
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : undefined
+  }
+
   const shouldShowCardAction = () => {
     return !!card?.memberIds?.length || !!card?.comments?.length || !!card?.attachments?.length
   }
 
   return (
-    <Card sx={{
-      cursor:'pointer',
-      boxShadow:'0 1px 1px rgba(0,0,0,0.2)',
-      overflow:'unset'
-    }}>
+    <Card
+      ref={setNodeRef}
+      style={dndKitColumnStyle}
+      {...attributes}
+      {...listeners}
+      sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0,0,0,0.2)',
+        overflow: 'unset'
+      }}>
       {card?.cover && <CardMedia sx={{ height: 140 }} image={card?.cover} />}
-      <CardContent sx={{ p:1.5, '&:last-child':{ p:1.5 } }}>
+      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
         <Typography>
           {card?.title}
         </Typography>
       </CardContent>
       {shouldShowCardAction() &&
-      <CardActions sx={{ p:'0px 4px 8px 4px' }}>
-        {!!card?.memberIds?.length && <Button size="small" startIcon={<GroupIcon/>}>{card?.memberIds?.length}</Button>}
-        {!!card?.comments?.length && <Button size="small" startIcon={<CommentIcon/>}>{card?.comments?.length}</Button>}
-        {!!card?.attachments?.length && <Button size="small" startIcon={<AttachmentIcon/>}>{card?.comments?.length}</Button>}
-      </CardActions>}
+        <CardActions sx={{ p: '0px 4px 8px 4px' }}>
+          {!!card?.memberIds?.length && <Button size="small" startIcon={<GroupIcon />}>{card?.memberIds?.length}</Button>}
+          {!!card?.comments?.length && <Button size="small" startIcon={<CommentIcon />}>{card?.comments?.length}</Button>}
+          {!!card?.attachments?.length && <Button size="small" startIcon={<AttachmentIcon />}>{card?.comments?.length}</Button>}
+        </CardActions>}
     </Card>
   )
 }
