1d69c7a18d7cf91b13932d3c8a0878e966aa6c0d
luong tran <luongluong99783@gmail.com>
2023-12-19 15:48:57 +0700

    mongodb_aggregate

--- src/pages/Board/BoardBar/BoardBar.jsx
+++ src/pages/Board/BoardBar/BoardBar.jsx
@@ -47,12 +47,14 @@ function BoardBar({ board }) {
           alignItems:'center',
           gap:1
         }}>
-          <Chip
-            sx={MENU_STYLES}
-            icon={<DashboardIcon />}
-            label={board?.title}
-            clickable
-          />
+          <Tooltip title={board?.description}>
+            <Chip
+              sx={MENU_STYLES}
+              icon={<DashboardIcon />}
+              label={board?.title}
+              clickable
+            />
+          </Tooltip>
           <Chip
             sx={MENU_STYLES}
             icon={<VpnLockIcon />}
