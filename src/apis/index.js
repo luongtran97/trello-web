57151fc658de49fad352d59390753bfeb1edbd13
luong tran <luongluong99783@gmail.com>
2023-12-21 16:41:59 +0700

    api_column_card_p1

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,20 @@
 import axios from 'axios'
 import { API_ROOT } from '~/utils/constant'
 
+/** Board */
 export const fetchBoardDetailsAPI = async(boardID) => {
   const request = await axios.get(`${API_ROOT}/v1/boards/${boardID}`)
   return request.data
+}
+
+/** Columns */
+export const createNewColumAPI = async(newColumData) => {
+  const response = await axios.post(`${API_ROOT}/v1/columns`, newColumData)
+  return response.data
+}
+
+/** Cards */
+export const createNewCardAPI = async(newCardData) => {
+  const response = await axios.post(`${API_ROOT}/v1/cards`, newCardData)
+  return response.data
 }
--- src/pages/Board/_id.jsx
+++ src/pages/Board/_id.jsx
@@ -6,20 +6,65 @@ import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 // import { mockData } from '~/apis/moc-data'
 import { fetchBoardDetailsAPI } from '~/apis'
+import { createNewColumAPI, createNewCardAPI } from '~/apis'
+import { generatePlaceHolderCard } from '~/utils/formatters'
+import { isEmpty } from 'lodash'
 function Board() {
   const [board, setBoard] = useState(null)
 
   useEffect(() => {
     const boardId = '6580f73fea92d11a4fd72e72'
-    fetchBoardDetailsAPI(boardId).then(( res ) => {
-      setBoard(res)
+    fetchBoardDetailsAPI(boardId).then( board => {
+      //cần xử lý vấn đề kéo thả vào một column rỗng
+      board.columns.forEach(((column) => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceHolderCard(column)]
+          column.cardOrderIds = [generatePlaceHolderCard(column)._id]
+        }
+      }))
+      setBoard(board)
     })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+  const handelAddNewColumn = async(newColumnTitle) => {
+    const newColumnData = {
+      title:newColumnTitle,
+      boardId:board._id
+    }
+
+    //cập nhật state board
+    const createdNewColumn = await createNewColumAPI(newColumnData)
+    //khi tạo 1 column mới chưa có card, cần xử lý vấn đề kéo thả vào một column rỗng
+    createdNewColumn.cards = [generatePlaceHolderCard(createdNewColumn)]
+    createdNewColumn.cardOrderIds = [generatePlaceHolderCard(createdNewColumn)._id]
+    const newBoard = { ...board }
+    newBoard.columns.push(createdNewColumn)
+    newBoard.columnOrderIds.push(createdNewColumn._id)
+    setBoard(newBoard)
+  }
+
+  const handelcreateNewCard = async(newCardData) => {
+    const createdCard = await createNewCardAPI({
+      ...newCardData,
+      boardId:board._id
+    })
+    const newBoard = { ...board }
+    const columnTuUpdate = newBoard.columns.find(column => column._id === createdCard.columnId )
+    if (columnTuUpdate) {
+      columnTuUpdate.cards.push(createdCard)
+      columnTuUpdate.cardOrderIds.push(createdCard._id)
+    }
+    setBoard(newBoard)
+  }
   return (
     <Container maxWidth={false} disableGutters sx={{ height:'100vh' }}>
       <AppBar/>
       <BoardBar board={board} />
-      <BoardContent board={board}/>
+      <BoardContent
+        board={board}
+        handelAddNewColumn={handelAddNewColumn}
+        handelcreateNewCard={handelcreateNewCard}
+      />
     </Container>
   )
 }
