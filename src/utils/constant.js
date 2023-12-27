9271a92ac751cc5abbfe13567114f8bc459d735f
luong tran <luongluong99783@gmail.com>
2023-12-27 22:12:33 +0700

    fix BUILD_MODE dev

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,10 @@
-export const API_ROOT = 'https://trello-api-l5it.onrender.com/'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8080'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-l5it.onrender.com/'
+}
+console.log('🚀 ~ apiRoot:', apiRoot)
+
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  define:{
+    'process.env': process.env
+  },
   plugins: [
     react(),
     svgr()
