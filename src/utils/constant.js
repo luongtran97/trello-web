80b8b797b824ee78e4bb32dad65430a74cce8348
luong tran <luongluong99783@gmail.com>
2023-12-27 22:26:56 +0700

    update apiRoot

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,7 +3,7 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8080'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-l5it.onrender.com/'
+  apiRoot = 'https://trello-api-l5it.onrender.com'
 }
 console.log('🚀 ~ apiRoot:', apiRoot)
 
