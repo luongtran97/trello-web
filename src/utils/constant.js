6e4b1316909807f566bd0219e5e34790a30cd465
luong tran <luongluong99783@gmail.com>
2023-12-27 22:33:56 +0700

    update

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,6 +5,6 @@ if (process.env.BUILD_MODE === 'dev') {
 if (process.env.BUILD_MODE === 'production') {
   apiRoot = 'https://trello-api-l5it.onrender.com'
 }
-console.log('🚀 ~ apiRoot:', apiRoot)
+
 
 export const API_ROOT = apiRoot
